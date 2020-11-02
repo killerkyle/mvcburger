12162de7af037b5db8e4a08c098212510785d27a
Kyle Pace <kylelpace@gmail.com>
2020-11-02 14:45:36 -0700

    password issues

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "BostoN!1987!",
+    password: "",
     database: "burgers_db"
   });
 }
