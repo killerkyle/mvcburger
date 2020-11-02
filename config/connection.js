fbb3d5129bd220797052521e438e1b018ba78902
Kyle Pace <kylelpace@gmail.com>
2020-11-02 16:32:54 -0700

    errors connecting to mysql and heroku after deployment with jaws

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,9 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
-    password: "",
+    password: "BostoN!1987!",
     database: "burgers_db"
   });
 }
